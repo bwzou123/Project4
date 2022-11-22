c9fc140f514faafd940d1d90c6f23dc4cc7c84b7
bwzou123 <111713470+bwzou123@users.noreply.github.com>
2022-11-22 15:03:12 -0500

    Add files via upload

---
+++ sudoku_generator.py
@@ -0,0 +1,101 @@
+def generate_sudoku(size, removed):
+    pass
+
+
+class SudokuGenerator:
+    def __init__(self, row_length, removed_cells):
+        self.row_length = row_length
+        self.removed_cells = removed_cells
+
+    def get_board(self):
+        pass
+
+    def print_board(self):
+        pass
+
+    def valid_in_row(self,row,num):
+        pass
+
+    def valid_in_col(self, col, num):
+        pass
+
+    def valid_in_box(self, row_start, col_start, num):
+        pass
+
+    def is_valid(self, row, col, num):
+        pass
+
+    def fill_box(self, row_start, col_start):
+        pass
+
+    def fill_diagonal(self):
+        pass
+
+    def fill_remaining(self, row, col):
+        pass
+
+    def fill_values(self):
+        pass
+
+    def remove_cells(self):
+        pass
+
+
+class Cell:
+    def __init__(self, value, row, col, screen):
+        pass
+
+    def set_cell_value(self, value):
+        pass
+
+    def set_sketched_value(self, value):
+        pass
+
+    def draw(self):
+        pass
+
+
+class Board:
+    def __init__(self, width, height, screen, difficulty):
+        pass
+
+    def draw(self):
+        pass
+
+    def select(self,row,col):
+        pass
+
+    def click(self,x,y):
+        pass
+
+    def clear(self):
+        pass
+
+    def sketch(self):
+        pass
+
+    def place_number(self, value):
+        pass
+
+    def reset_to_original(self):
+        pass
+
+    def is_full(self):
+        pass
+
+    def update_board(self):
+        pass
+
+    def find_empty(self):
+        pass
+
+    def check_board(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
